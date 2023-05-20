ec26e94b1bed9d364a1ee03a4b92dc0c3a87038e
AiceIceCream <manuelhumphrey921@gmail.com>
2023-05-19 22:06:28 -0700

    site 2 lab 2

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request; //Handling http request from lumen
+use App\Models\User; //My Model
+use App\Traits\ApiResponser; //Standard API response
+use DB; // can be use if not using eloquent, you can use DB component in lumen
+
+use Illuminate\Http\Response;
+
+Class UserController extends Controller {
+    use ApiResponser;
+
+    protected $primaryKey = 'userId';
+    private $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+    public function getUsers()
+    {
+        // Eloquent Style
+        // $users = User::all();
+
+        // sql string as parameter
+        $users = DB::connection('mysql')
+        ->select("Select * from tbluser");
+
+        return $this -> successReponse($users);
+    }
+
+    public function index(){
+        $users = User::all();
+        return $this -> successReponse($users);
+    }
+    
+    public function add(Request $request){
+        
+        $rules = [
+            'username' => 'required|max:50',
+            'password' => 'required|max:50',
+            'gender' => 'required|in:Male,Female',
+        ];
+
+        $this->validate($request,$rules);
+
+        $users = User::create($request->all());
+        
+        return $this -> successReponse($users, Response::HTTP_CREATED);
+    }
+
+    public function update(Request $request, $id){
+        $rules = [
+            'username' => 'required|max:50',
+            'password' => 'required|max:50',
+            'gender' => 'required|in:Male,Female',
+        ];
+
+        $this->validate($request ,$rules);
+        $users = User::where('userId', $id)->firstOrFail();
+        $users->fill($request->all());
+
+        if($users->isClean()){
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        $users->save();
+        return $users;
+    }
+
+    public function delete($id){
+        $users = User::FindOrFail($id);
+        $users->delete();
+        return $this->successResponse($users);
+    }
+
+    public function show($id){
+        $users = User::FindOrFail($id);
+            return $this -> successReponse($users);
+        }
+    } 
